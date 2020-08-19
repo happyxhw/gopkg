8235e0c20acb14538e15b25121057656e07e5d88
happyxhw <happyxhw@outlook.com>
2020-08-19 17:49:13 +0800

    change config

--- config/config.go
+++ config/config.go
@@ -3,53 +3,26 @@ package config
 import (
 	"os"
 
-	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/viper"
 )
 
-func SetupConfig(configPath string) (*viper.Viper, error) {
-	v, err := initConfig(configPath)
-	return v, err
-}
-
-func initConfig(configPath string) (*viper.Viper, error) {
-	v := viper.New()
+// InitConfig init config
+func InitConfig(configPath string) error {
 	if os.Getenv("ENV") == "DEV" {
-		v.SetConfigName("dev")
+		viper.SetConfigName("dev")
 	} else if os.Getenv("ENV") == "TEST" {
-		v.SetConfigName("test")
+		viper.SetConfigName("test")
 	} else if os.Getenv("ENV") == "PROD" {
-		v.SetConfigName("prod")
+		viper.SetConfigName("prod")
 	} else {
 		panic("wrong env not in [DEV, TEST, PROD]")
 	}
-	v.AddConfigPath(configPath)
-	v.SetConfigType("yaml")
+	viper.AddConfigPath(configPath)
+	viper.SetConfigType("yaml")
 
-	if err := v.ReadInConfig(); err != nil {
-		return nil, err
+	if err := viper.ReadInConfig(); err != nil {
+		return err
 	}
 
-	return v, nil
-}
-
-func Decode(src, dst interface{}) {
-	if src == nil {
-		panic("no config found")
-	}
-	err := mapstructure.Decode(src, dst)
-	if err != nil {
-		panic(err)
-	}
-	if dst == nil {
-		panic("no config result")
-	}
-}
-
-func Get(v *viper.Viper, key string) interface{} {
-	c := v.Get(key)
-	if c == nil {
-		panic("no config found")
-	}
-	return c
+	return nil
 }
--- config/config_test.go
+++ config/config_test.go
@@ -4,32 +4,16 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/spf13/viper"
 )
 
 func TestSetupConfig(t *testing.T) {
 	_ = os.Setenv("ENV", "DEV")
-	v, err := SetupConfig(".")
+	err := InitConfig(".")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(v.Get("db"))
-}
-
-func TestDecode(t *testing.T) {
-	type db struct {
-		Host int
-		User int
-	}
-
-	var d db
-
-	_ = os.Setenv("ENV", "PROD")
-	v, err := SetupConfig(".")
-	if err != nil {
-		panic(err)
-	}
-
-	Decode(v.Get("db"), &d)
-	fmt.Printf("%+v\n", d)
+	fmt.Println(viper.Get("db"))
 }
