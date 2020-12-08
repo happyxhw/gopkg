ce7508e108e6e4232e6a9629521621aeb8001a9d
happyxhw <happyxhw@outlook.com>
2020-12-08 18:02:52 +0800

    change config

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"os"
 
+	"github.com/happyxhw/gopkg/logger"
 	"github.com/spf13/viper"
 )
 
@@ -15,7 +16,7 @@ func InitConfig(configPath string) error {
 	} else if os.Getenv("ENV") == "PROD" {
 		viper.SetConfigName("prod")
 	} else {
-		panic("wrong env not in [DEV, TEST, PROD]")
+		logger.Fatal("wrong env not in [DEV, TEST, PROD]")
 	}
 	viper.AddConfigPath(configPath)
 	viper.SetConfigType("yaml")
